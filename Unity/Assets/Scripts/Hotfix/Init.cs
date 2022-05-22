a1e88f579943b9967705762ed7bd190b03ceaa2a
RT <2760764439@qq.com>
2022-05-22 20:32:22 +0800

    更改整个项目的目录结构

---
+++ Unity/Assets/Scripts/Hotfix/Init.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Hotfix
+{
+    public class Init
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
